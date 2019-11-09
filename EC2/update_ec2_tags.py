5ceb550e5e6c3ce05e7f8ee2fc5d79b98391b195
Satish Naidi <SatishNaidi@gmail.com>
2019-11-09 18:12:05 +0530

    Update update_ec2_tags.py

--- EC2/update_ec2_tags.py
+++ EC2/update_ec2_tags.py
@@ -1,6 +1,6 @@
 import boto3
 import botocore
-import sys
+import sys, json
 
 
 def ec2_list_of_instances(ec2_conn_obj):
@@ -11,13 +11,13 @@ def ec2_list_of_instances(ec2_conn_obj):
     tagname = "Patch Group"
     try:
         ec2_describe_response = ec2_conn_obj.describe_instances()
-    except  botocore.exceptions.EndpointConnectionError as err:
+    except botocore.exceptions.EndpointConnectionError as err:
         print("Error:- Couldn't connect to the internet Please check the network setting")
         sys.exit(1)
     all_inst_dict_tolist = []
     for reservation in ec2_describe_response["Reservations"]:
         for instance in reservation["Instances"]:
-            each_inst_dict = {}
+            each_inst_dict = dict()
             each_inst_dict["InstanceId"] = instance["InstanceId"]
             if instance.get("Platform") == "windows":
                 tag_value = "WINDOWSTAGGOESHERE"
@@ -46,11 +46,29 @@ def add_tags(ec2_conn_obj,tag_info):
     return response
 
 
-if __name__ == "__main__":
-    ec2_conn_obj = boto3.client('ec2')
+
+def lambda_handler(event,context):
+    try:
+        ec2_conn_obj = boto3.client('ec2')
+    except botocore.exceptions.NoCredentialsError:
+        print("Unable to locate default credentials. Configure credentials")
+        ec2_conn_obj = boto3.client(
+            'ec2',
+            aws_access_key_id="",
+            aws_secret_access_key=""
+        )
     tags_info = ec2_list_of_instances(ec2_conn_obj)
+    response = []
     for each_item in tags_info:
         if each_item["to_be_added_tag"].items() <= each_item["existing_tags"].items():
-            print("Tags Exists for " + each_item["InstanceId"])
+            response.append("Tags Exists for " + each_item["InstanceId"])
         else:
-            print(add_tags(ec2_conn_obj, each_item))
+            response.append(add_tags(ec2_conn_obj, each_item))
+    return {
+        'statusCode': 200,
+        'body': json.dumps(str(response))
+    }
+
+
+if __name__ == "__main__":
+    print(lambda_handler({}, {}))
